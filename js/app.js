8c77701fd084a55275bc8e82870fdff2033dd90e
nuhuadamuisah <147081701+nuhuadamuisah@users.noreply.github.com>
2023-10-17 16:46:16 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+/*=================typying animation=================*/
+var typed = new Typed(".typing", {
+    strings: ["Web Designer", "React Developer", "Data Analyst", "Cybersecurity Enthusiast"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 1000,
+    startDelay: 1000,
+    loop: true,
+ });
+
+ var typed = new Typed(".typing-2", {
+    strings: ["Web Designer", "React Developer", "Data Analyst", "Cybersecurity Enthusiast"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 1000,
+    startDelay: 1000,
+    loop: true,
+ });
